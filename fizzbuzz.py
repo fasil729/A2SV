e6a0fcaabce482e6473a86e494ed22361151c74f
fasika <fasikafikadu38@gmail.com>
2022-11-04 19:14:02 +0300

    fizz buzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,1 +1,13 @@
-print("github is good thing")
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        List = []
+        for i in range(1, n+1):
+            if i % 3 == 0 and i % 5 == 0:
+                List.append("FizzBuzz")
+            elif i % 3 == 0:
+                List.append("Fizz")
+            elif i % 5 == 0:
+                List.append("Buzz")
+            else:
+                List.append(str(i))
+        return List
